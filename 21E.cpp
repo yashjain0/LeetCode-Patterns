da0c4348a71143cf7d52c1df3632d3e531308a4d
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-04-22 21:46:05 +0530

    Merging linked list

---
+++ 21E.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL) return l2;
+        if(l2==NULL) return l1;
+      ListNode* l3=NULL;
+        //find first element
+        if(l1->val<l2->val) {l3=l1; l1=l1->next;}
+        else{l3=l2; l2=l2->next;}
+        //can use
+        /*
+        Using list dummy 
+        ListNode dummy(INT_MIN);
+        ListNode *tail = &dummy;
+        */
+     ListNode* p=l3;
+        
+      while(l1 && l2)
+      {
+          if(l1->val<l2->val)
+          {
+              p->next=l1;
+              l1=l1->next;
+                  
+          }
+          else
+          {
+              p->next=l2;
+              l2=l2->next;
+                  
+          }
+          p=p->next;
+      }
+    /* 
+    tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    */
+    if(l1) p->next=l1;
+    else p->next=l2;
+    return l3;
+    }
+};
